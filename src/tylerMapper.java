1c76f0cde824006e2a892a79270a79b3d1c13f35
Tyler Daspit <54476633+tdaspit@users.noreply.github.com>
2022-03-19 11:00:38 -0500

    needed-imports

--- src/tylerMapper.java
+++ src/tylerMapper.java
@@ -1,22 +1,29 @@
 // building my own mapper while waiting on my team mates to communicate with me
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import java.util.*;
 
 public class tylerMapper {
 
-	List<List<String>> records = new ArrayList<>();
+	List<List<String>> records1 = new ArrayList<>();
 	try (BufferedReader br = new BufferedReader(new FileReader("input.csv"))) {
 	    String line;
 	    while ((line = br.readLine()) != null) {
 	        String[] values = line.split(COMMA_DELIMITER);
-	        records.add(Arrays.asList(values));
+	        records1.add(Arrays.asList(values));
 	    }
 	}
 	
-	List<List<String>> records = new ArrayList<>();
+	List<List<String>> records1 = new ArrayList<>();
 	try (Scanner scanner = new Scanner(new File("input.csv"));) {
 	    while (scanner.hasNextLine()) {
-	        records.add(getRecordFromLine(scanner.nextLine()));
+	        records1.add(getRecordFromLine(scanner.nextLine()));
 	    }
 	}
 	
@@ -35,7 +42,7 @@ public class tylerMapper {
 	try (CSVReader csvReader = new CSVReader(new FileReader("book.csv"));) {
 	    String[] values = null;
 	    while ((values = csvReader.readNext()) != null) {
-	        records.add(Arrays.asList(values));
+	        records1.add(Arrays.asList(values));
 	    }
 	}
 	
